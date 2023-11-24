54df2c2c2bc0e4370472f33e2d5ab7a9d3b176d8
Laksh_tech <lakshdeep@itechnolabs.tech>
2023-11-24 22:50:03 +0530

    fixes

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::middleware('prevent-back-history')->group(function (){
         Route::get('/user-logout', 'Auth\LoginController@userLogout')->name('userLogout');
         Route::post('/add/prescription', 'UserController@addPrescription')->name('addPrescription');
         Route::post('/get/prescription/list', 'UserController@getTraumaData')->name('getTraumaData');
-        Route::post('/get/card', 'UserController@deleteTraumaCard')->name('deleteTraumaCard');
+        Route::post('/delete/card', 'UserController@deleteTraumaCard')->name('deleteTraumaCard');
 
     });
     
