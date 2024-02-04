c8a7601ab8bfd5be16f9f51d54456796ccf2b819
Sourabh <saurav7767@gmail.com>
2024-02-04 16:12:35 +0530

    updates

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -1,5 +1,6 @@
 <html>
     <head>
+        <title>Avacadokiwi</title>
         <!-- <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" 
         rel="stylesheet" type="text/css" /> -->
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" 
