cbefa2ab492209677961cefcaca5151d72a50e63
Laksh_tech <lakshdeep@itechnolabs.tech>
2024-10-21 21:55:19 +0530

    livewire coment

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -22,7 +22,6 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-  {{-- @livewireStyles --}}
   <style>
     .item-content {
     /* padding: 10px; */
